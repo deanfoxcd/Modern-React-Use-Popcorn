93eee9d4b7f6261cd3046811c5682b0b0bd0acdc
Dean Fox <deanfoxcd@gmail.com>
2024-11-27 20:08:33 -0500

    add useEffect for fetching movies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import './StarRating';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App-v1';
+// import './StarRating';
+// import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} color={'red'} size={24} /> */}
   </React.StrictMode>
 );
